47f017f5a3ef09f9ace9acc9746459a2fea754e2
ryan <yaho4752@gmail.com>
2024-12-28 17:33:45 +0900

    Add new solution for 다리를 지나는 트럭 programming challenge

---
+++ Programmers/다리를 지나는 트럭.py
@@ -0,0 +1,18 @@
+from collections import deque
+
+def solution(bridge_length, weight, truck_weights):
+    answer = 0
+    bridge = deque([0] * bridge_length)
+    current_weight = 0
+    
+    while bridge:
+        answer += 1
+        current_weight -= bridge.popleft()
+        if truck_weights:
+            if current_weight + truck_weights[0] <= weight:
+                truck = truck_weights.pop(0)
+                bridge.append(truck)
+                current_weight += truck
+            else:
+                bridge.append(0)
+    return answer
